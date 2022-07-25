891594c296de54ef87025030fd332557376141db
Kapil Pokhrel <kapilpokhrel23@gmail.com>
2022-07-25 13:56:13 +0545

    static pages

---
+++ Practice/Application.cs
@@ -0,0 +1,17 @@
+using system;
+namespace OOP
+{
+    public class Application
+    
+    {
+    Task[] tasks:
+    public static void main()
+    {
+        Task task 1 = new Task("Go to the market");
+        Task task 2 = new Task("Go to the market");
+        Task task 3 = new Task("Go to the market");
+        Task task 4 = new Task("Go to the market");
+
+    }
+    }
+}
